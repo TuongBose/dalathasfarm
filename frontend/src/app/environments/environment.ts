8cd9d5210c4318735d1101f35f71903b52e26f87
Le Manh Tuong <lemanhtuong2109@gmail.com>
2025-11-30 04:31:29 +0700

    update enpoint

--- frontend/src/app/environments/environment.ts
+++ frontend/src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiBaseUrl: 'http://localhost:8080/api/v1',
-    // apiBaseUrl: 'http://localhost:8099/api/v1',
+    // apiBaseUrl: 'http://localhost:8080/api/v1',
+    apiBaseUrl: 'http://localhost:8099/api/v1',
     // apiBaseUrl: 'https://vps.dalathasfarm.x10.mx/api/v1',
 }
